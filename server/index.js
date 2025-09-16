d33fbb6501816c6dfaae70daed52ea809868694e
mahmedraza1 <developer.mahmedraza@gmail.com>
2025-09-16 21:31:44 +0500

    add static file serving

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
@@ -302,7 +302,17 @@ app.put('/learnlive/lectures/:lectureId', (req, res) => {
   }
 });
 
+// Serve static files from the React app build directory
+app.use(express.static(path.join(__dirname, '..', 'dist')));
+
+// The "catch all" handler: for any request that doesn't match API routes above, 
+// send back React's index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+});
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`Serving frontend from ${path.join(__dirname, '..', 'dist')}`);
 });
